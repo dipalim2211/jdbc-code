a4323a5c3fc470b487d4372a4a3a2d20e249539c
dipalim2211 <dipalimanere22@gmail.com>
2025-03-27 22:10:44 +0530

    CRUD Update DabaseOperation code

--- jdbc-connection-firstDemo/src/main/java/org/example/jdbc/DatabaseOperation.java
+++ jdbc-connection-firstDemo/src/main/java/org/example/jdbc/DatabaseOperation.java
@@ -13,10 +13,12 @@ public class DatabaseOperation {
     public static void main(String[] args) {
 
         connectToDBAndRetrieveResult();
-        addStudents(9 ,"Sonali",22); //Pass student data to add
+       // addStudents(9 ,"Sonali",22);   //Pass student data to add
+        updateStudents(9,"Monali",21); //Pass student data to update
 
     }
 
+    //CRUD:READ
     private static void connectToDBAndRetrieveResult() {
 
         final String SELECT_QUERY = "select * from students";
@@ -40,37 +42,18 @@ public class DatabaseOperation {
         }
     }
 
+    //CRUD:CREATE
     private static void addStudents(int id,String name,int age) {
 
-        /*
-        final String insertQuery = "insert into students(id,name,age) values ("+id+",'"+name+"',"+age+")";
-        query used by statement obj and issue may occur due to this.
-        issue:
-        1. SQL injection Attack('""') : Not recommended Approach
-        2.it's slow : Because of dynamic value(ex. 1000 times insertion by dynamic value)
-        you cant catch the query
-        3.Syntax complex to manage
-
-        to Overcome,
-        Java introduced new statement : PreparedStatement
-        1.No SQL injection Attack
-        2.it's fast
-        3.syntax is simple
-        4.instead of dynamic values use same count place holder(?)
-        5.use preparedStatement methode instead statement method and pass query
-
-         */
         final String insertQuery = "insert into students(id,name,age) values (?,?,?)"; //4.
 
         System.out.println("Query Inserted : "+insertQuery);
 
-
         try(Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             PreparedStatement statement = connection.prepareStatement(insertQuery);)   //5.
         {
-            //For first ? I want id
+
             statement.setInt(1,id);
-            //set name for second ?
             statement.setString(2,name);
             statement.setInt(3,age);
 
@@ -80,6 +63,36 @@ public class DatabaseOperation {
                 System.out.println("Student Record Inserted");
             }else{
                 System.out.println("Student Record Not Inserted");
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //CRUD : UPDATE
+    private static void updateStudents(int id,String name,int age) {
+
+        final String updateQuery = "update students set age = ? , name = ? where id = ?";
+
+        System.out.println("Query Inserted : "+updateQuery);
+
+        try(Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement statement = connection.prepareStatement(updateQuery);)   //5.
+        {
+
+            //Arrange as per placeholder ?
+            statement.setInt(1,age);
+            statement.setString(2,name);
+            statement.setInt(3,id);
+
+            int count = statement.executeUpdate();
+
+            if(count>0){
+                System.out.println("Student Record Updated");
+            }else{
+                System.out.println("Student Record Not Updated");
             }
 
         }catch (Exception e) {
